b67d3d98a89e62dae3bd15abb2728f0884a53b42
wioenena-q <wioenena@gmail.com>
2022-08-19 18:47:31 +0300

    chore: removed deno support

--- src/deno/mod.ts
+++
@@ -1,4 +0,0 @@
-export { DISCORD_EPOCH, TWITTER_EPOCH } from '../constants.ts';
-export { DiscordSnowflake } from '../DiscordSnowflake.ts';
-export { Snowflake, type ParsedSnowflake } from '../Snowflake.ts';
-export { TwitterSnowflake } from '../TwitterSnowflake.ts';
