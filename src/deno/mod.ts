cf28f8b923e5a20e3922ab4e86800fe74b62da55
wioenena-q <wioenena@gmail.com>
2022-08-19 18:34:01 +0300

    feat: deno support added

---
+++ src/deno/mod.ts
@@ -0,0 +1,4 @@
+export { DISCORD_EPOCH, TWITTER_EPOCH } from '../constants.ts';
+export { DiscordSnowflake } from '../DiscordSnowflake.ts';
+export { Snowflake, type ParsedSnowflake } from '../Snowflake.ts';
+export { TwitterSnowflake } from '../TwitterSnowflake.ts';
